7cd358d31540cdc05c144c1230c804fb28e992c8
Daniel Roe <daniel@roe.dev>
2022-04-05 11:29:51 +0100

    fix: update dependencies

--- my.test.ts
+++ my.test.ts
@@ -6,13 +6,12 @@ const playground = fileURLToPath(new URL('./playground', import.meta.url))
 
 await setup({
   rootDir: playground,
-  runner: 'vitest',
   server: true
 })
 
 describe('Sample test suite', () => {
   it('Sanity image builder works', async () => {
-    const { body: html } = await $fetch('/')
+    const html = await $fetch('/')
     expect(html).toContain(
       'Nuxt module playground'
     )
